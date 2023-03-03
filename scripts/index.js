4a972fe16f75c0142e2be68a4aaaaaa0528b6927
Ivan C <Ivan_Cosgrove@email.com>
2023-03-03 09:53:11 -0500

    feat: program cards through Java

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+//Modal Box Code
+const modalBox = document.querySelector(".modal-box");
+const modalBoxOpen = document.querySelector(".modal-box_opened");
+const modalClose = document.querySelector(".modal-box__button");
+const editButton = document.querySelector(".profile__buttons-edit");
+const profileName = document.querySelector(".profile__title");
+const profileDesc = document.querySelector(".profile__subtitle");
+const nameValue = document.querySelector(".form__input_type_name");
+const descValue = document.querySelector(".form__input_type_description");
+
+editButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  nameValue.value = profileName.textContent;
+  descValue.value = profileDesc.textContent;
+  modalBox.classList.toggle("modal-box_opened");
+});
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalBox.classList.toggle("modal-box_opened");
+});
+modalBox.addEventListener("submit", function formSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameValue.value;
+  profileDesc.textContent = descValue.value;
+  modalBox.classList.toggle("modal-box_opened");
+});
+
+//Cards Code
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -25,16 +53,19 @@ const initialCards = [
   },
 ];
 
-const modalBox = document.querySelector(".modal-box");
-const modalBoxOpen = document.querySelector(".modal-box_opened");
-const modalClose = document.querySelector(".modal-box__button");
-const editButton = document.querySelector(".profile__buttons-edit");
-
-editButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalBox.classList.toggle("modal-box_opened");
-});
-modalClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalBox.classList.toggle("modal-box_opened");
-});
+function getCardTemplate(data) {
+  const cardList = document.querySelector(".cards__list");
+  const cardTemplate = document.querySelector("#card").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  let cardName = cardElement.querySelector(".card__title");
+  let cardImage = cardElement.querySelector(".card__image");
+  let cardCap = initialCards.name;
+  let cardImg = initialCards.link;
+  cardName.textContent = cardCap;
+  cardImage.src = cardImg;
+  cardImage.alt = cardCap;
+  cardList.append(cardElement);
+}
+for (let card of initialCards) {
+  getCardTemplate(card);
+}
